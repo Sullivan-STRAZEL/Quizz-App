5764c3577ca506a4920af3c0ca4ee93f070e5547
Sullivan STRAZEL <strazel.sullivan@hotmail.fr>
2022-12-26 19:21:56 +0100

    Initialisation du projet avec HTML et JS

---
+++ app.js
@@ -0,0 +1,21 @@
+// responses = ["c", "a", "b", "a", "c"];
+const responses = [];
+const emojis = ["✔️", "✨", "👀", "😭", "👎"];
+
+const form = document.querySelector("form");
+const button = document.querySelector("button");
+
+const questionContainer1 = document.querySelector(".question-container-1");
+const questionContainer2 = document.querySelector(".question-container-2");
+const questionContainer3 = document.querySelector(".question-container-3");
+const questionContainer4 = document.querySelector(".question-container-4");
+const questionContainer5 = document.querySelector(".question-container-5");
+
+const option = questionContainer.querySelectorAll("input[type='radio']");
+
+button.addEventListener("click", e => {
+    e.preventDefault();
+
+    if ()
+})
+
